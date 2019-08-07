5a643f22fcd60880e868d37079356e47b3ac5dfb
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-06 21:30:03 -0400

    Env variables set. Connected to MongoDB with mongoose

--- ic-server/src/server.js
+++ ic-server/src/server.js
@@ -2,10 +2,29 @@ const mongoose = require('mongoose');
 const express = require('express');
 
 const bodyParser = require('body-parser');
-const PORT = 3001;
+
+if (process.env.NODE_ENV !== 'production') {
+    const results = require('dotenv').config();
+    console.log("In Dev mode, results : ", results)
+}
+
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+mongoose.connect(MONGODB_URL, {useNewUrlParser : true}, (err) => {
+    if(err) {
+        console.log("Mongo connection error, ", err);
+    } else {
+        console.log("Successfully connected to database")
+    }
+})
 
 const app = express();
 const router = express.Router();
+
+
+
+
 
 app.listen(PORT, () => {
     console.log("Backend up at Port 3001")
