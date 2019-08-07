2c010557c2bde2dd1bf853d86d9278c188145d06
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-07 17:44:47 -0400

    Create & Passed tests for Create, Dashboard

---
+++ ic-client/src/tests/Create.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Create from '../components/Create';
+
+test("Should render Create correctly", () => {
+    const wrapper = shallow(<Create />);
+    expect(wrapper).toMatchSnapshot();
+});
