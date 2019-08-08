6f770c3423e5ef1d7cde91118af8ddadef880b7e
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-07 23:53:02 -0400

    Redux store up

--- ic-client/src/store/store.js
+++ ic-client/src/store/store.js
@@ -1,1 +1,10 @@
 import { createStore, combineReducers } from redux;
+import instantCoffeeReduer from '../store/store';
+
+const store = createStore(
+    combineReducer({
+        instantCoffee: instantCoffeeReduer
+    })
+)
+
+export default store;
