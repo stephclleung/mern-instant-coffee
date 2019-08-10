7ca1a3a4382a9c5db416dde458fed7d9d6aa139c
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 16:10:42 -0400

    Actions/instant-coffee : set default values

--- ic-client/src/actions/instant-coffee.js
+++ ic-client/src/actions/instant-coffee.js
@@ -6,23 +6,24 @@ export const addInstantCoffee = (instantCoffee) => ({
     instantCoffee
 });
 
-/**.
- *{
-    coffeeName = null,
-    packageSize = 0,
-    containerSize = 0,
-    price = -1,
-    currency = "invalid",
-    acidity = -1,
-    aroma = -1
-
- */
-
  //Returning a function, with dispatch as arguement
  //where upon the execution of this function
  // you return database post results for promise chaining (see test case )
  // without return axios... , test case cannot access the response data.
-export const addInstantCoffeeToDB = ( instantCoffee ) => {
+export const addInstantCoffeeToDB = ( data ) => {
+
+    const {
+        coffeeName = null,
+        packageSize = 0,
+        containerSize = 0,
+        price = -1,
+        currency = "invalid",
+        acidity = -1,
+        aroma = -1
+    } = data;
+
+    const instantCoffee = { coffeeName, packageSize, containerSize, price, currency, acidity, aroma };
+    console.log("Actions, " , instantCoffee )
     return (dispatch) => {
         return axios.post('http://localhost:5001/coffee/', instantCoffee)
         .then((res) => {
