13473f67d05f9023b52098ade6fbfc5f5fd3dfa0
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 17:18:41 -0400

    Component/InstantcoffeeForm : New test case for incorrect submissions

--- ic-client/src/tests/components/InstantCoffeeForm.test.js
+++ ic-client/src/tests/components/InstantCoffeeForm.test.js
@@ -7,3 +7,14 @@ test('should render Coffee form correctly', () => {
     const wrapper = shallow(<InstantCoffeeForm/>)
     expect(wrapper).toMatchSnapshot();
 })
+
+test('Should render error for form submissions with missing stuff', () => {
+    const wrapper = shallow(<InstantCoffeeForm/>);
+    expect(wrapper).toMatchSnapshot();
+
+    wrapper.find('form').simulate('submit', {
+        preventDefault: () => { }
+    });
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+    expect(wrapper).toMatchSnapshot();
+})
