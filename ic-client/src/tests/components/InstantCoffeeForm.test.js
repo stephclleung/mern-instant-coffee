53563f6edb2138962e64c0389871efa3382ef372
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 17:30:26 -0400

    Component/InstantcoffeeForm : Test case for price change / name change

--- ic-client/src/tests/components/InstantCoffeeForm.test.js
+++ ic-client/src/tests/components/InstantCoffeeForm.test.js
@@ -18,3 +18,35 @@ test('Should render error for form submissions with missing stuff', () => {
     expect(wrapper.state('error').length).toBeGreaterThan(0);
     expect(wrapper).toMatchSnapshot();
 })
+
+test("Should set coffeeName on change", () => {
+    const coffeeName = "I am a coffee name";
+    const wrapper = shallow(<InstantCoffeeForm/>);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('input').at(0).simulate('change', {
+        target : { value : coffeeName }
+    });
+    expect(wrapper.state('coffeeName')).toBe(coffeeName)
+
+})
+
+test("Should not change price if invalid input", () => {
+    const price = "I am a coffee name";
+    const wrapper = shallow(<InstantCoffeeForm/>);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('input').at(2).simulate('change', {
+        target : { value : price }
+    });
+    expect(wrapper.state('price')).toBe('')
+});
+
+
+test("Should change price if input is valid", () => {
+    const price = 123;
+    const wrapper = shallow(<InstantCoffeeForm/>);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.find('input').at(2).simulate('change', {
+        target : { value : price }
+    });
+    expect(wrapper.state('price')).toBe(123)
+})
