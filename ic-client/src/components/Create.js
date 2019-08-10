530efad8e452e4e01a196da2cf753b8decb08893
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 18:25:10 -0400

    Component/Create : switching to class base

--- ic-client/src/components/Create.js
+++ ic-client/src/components/Create.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import InstantCoffeeForm from './InstantCoffeeForm';
 
-const Create = () => {
-    return (
-        <div>
-            <InstantCoffeeForm />
-            <Link to="/">Back</Link>
-        </div>
-    );
+//TODO : dispatch "add" when InstantCoffeeForm submits.
+class Create extends React.Component {
+   
+    
+    render() {
+        return (
+            <div>
+                <InstantCoffeeForm />
+                <Link to="/">Back</Link>
+            </div>
+        );
+    }
 };
 
 export default Create;
