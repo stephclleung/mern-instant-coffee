0a5c43190b204f8c69a0d943d345c5c199cc6f5a
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 22:53:24 -0400

    Component/Edit : Change to class base. Need props and dispatch

--- ic-client/src/components/Edit.js
+++ ic-client/src/components/Edit.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import InstantCoffeeForm from './InstantCoffeeForm';
-const Edit = () => {
-    return (
-        <div>
-            <InstantCoffeeForm />
-        </div>
-    )
+
+//TODO: 
+// - Change to class base (props)
+// - Connect to store : dispatch and props
+// - Add remove button
+// 
+
+export class Edit extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <InstantCoffeeForm />
+            </div>
+        )
+    }
+
 }
 
 export default Edit;
