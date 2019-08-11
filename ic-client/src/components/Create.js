5ca52e2404c49b7ef1b6c6f5c713265cfa153462
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 22:42:09 -0400

    Component/Create: Access to store, can dispatch add

--- ic-client/src/components/Create.js
+++ ic-client/src/components/Create.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import InstantCoffeeForm from './InstantCoffeeForm';
+import { addInstantCoffee } from '../actions/instant-coffee';
+import { connect } from 'react-redux';
 
-//TODO : dispatch "add" when InstantCoffeeForm submits.
-class Create extends React.Component {
-   
-    
+export class Create extends React.Component {
+    onSubmit = (coffeeData) => {
+        this.props.addInstantCoffee(coffeeData);
+        this.props.history.push('/');
+    }
     render() {
         return (
             <div>
-                <InstantCoffeeForm />
+                <InstantCoffeeForm onSubmit={this.onSubmit} />
                 <Link to="/">Back</Link>
             </div>
         );
     }
 };
 
-export default Create;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        addInstantCoffee: (coffeeData) => dispatch(addInstantCoffee(coffeeData))
+    }
+}
+
+export default connect(undefined, mapDispatchToProps)(Create);
