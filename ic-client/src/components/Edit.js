caf37e358b098d7e539a6a8d53ce989ff68543ae
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-10 23:08:04 -0400

    Component/Edit : Remove function

--- ic-client/src/components/Edit.js
+++ ic-client/src/components/Edit.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import InstantCoffeeForm from './InstantCoffeeForm';
+import { connect } from 'react-redux';
+import { removeInstantCoffee } from '../actions/instant-coffee';
 
 //TODO: 
 // - Change to class base (props)
@@ -8,15 +10,29 @@ import InstantCoffeeForm from './InstantCoffeeForm';
 // 
 
 export class Edit extends React.Component {
-
+    onRemoveCoffee = () => {
+        this.props.removeInstantCoffee(this.props.instantCoffee.id)
+        this.props.history.push('/')
+    }
     render() {
         return (
             <div>
                 <InstantCoffeeForm />
+                <button onClick={this.onRemoveCoffee}>Remove Coffee</button>
             </div>
         )
     }
-
 }
 
-export default Edit;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        removeInstantCoffee: (coffee) => dispatch(removeInstantCoffee(coffee))
+    }
+}
+
+const mapStateToProps = (state, props) => ({
+    instantCoffee: state.instantCoffee.find((ic) => ic.id === props.match.params.id)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Edit);
+//export default Edit;
