1cefb93dbf4bd8dac093a63b96cd43cd4d21cab5
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-12 16:18:00 -0400

    Index : cleaning up test data, logs. Loading data from db onwards

--- ic-client/src/index.js
+++ ic-client/src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 //Store:
 import setStore from './store/store';
-import { addInstantCoffee } from './actions/instant-coffee';
+import { loadInstantCoffeeFromDB } from './actions/instant-coffee';
 
 import { instantCoffee } from './tests/fixtures/instant-coffee-data';
 const store = setStore();
@@ -16,21 +16,21 @@ const store = setStore();
 console.log("Checking store: ", store.getState())
 //TODO: remove this :
 
-const testData = () => {
-    for (let i = 0; i < 4; i++) {
-        store.dispatch(addInstantCoffee({
-            id: i.toString(),
-            coffeeName: instantCoffee[i].coffeeName,
-            packageSize: instantCoffee[i].packageSize,
-            containerSize: instantCoffee[i].containerSize,
-            price: instantCoffee[i].price,
-            currency: instantCoffee[i].currency,
-            acidity: instantCoffee[i].acidity,
-            aroma: instantCoffee[i].aroma
-        }));
-    }
-}
-testData();
+// const testData = () => {
+//     for (let i = 0; i < 4; i++) {
+//         store.dispatch(addInstantCoffee({
+//             id: i.toString(),
+//             coffeeName: instantCoffee[i].coffeeName,
+//             packageSize: instantCoffee[i].packageSize,
+//             containerSize: instantCoffee[i].containerSize,
+//             price: instantCoffee[i].price,
+//             currency: instantCoffee[i].currency,
+//             acidity: instantCoffee[i].acidity,
+//             aroma: instantCoffee[i].aroma
+//         }));
+//     }
+// }
+// testData();
 
 console.log("Checking store: (2nd)", store.getState())
 //TODO: remove this :
@@ -39,7 +39,13 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
-ReactDOM.render(jsx, document.getElementById('root'));
+
+ReactDOM.render(<p>..........</p>, document.getElementById('root'));
+
+store.dispatch(loadInstantCoffeeFromDB()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('root'));
+});
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
