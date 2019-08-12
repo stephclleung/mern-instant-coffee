cf4c624c5df479977181fbe8c90f58414ba22496
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-12 16:18:09 -0400

    Index : cleaning up test data, logs. Loading data from db onwards

--- ic-client/src/index.js
+++ ic-client/src/index.js
@@ -10,29 +10,10 @@ import { Provider } from 'react-redux';
 import setStore from './store/store';
 import { loadInstantCoffeeFromDB } from './actions/instant-coffee';
 
-import { instantCoffee } from './tests/fixtures/instant-coffee-data';
 const store = setStore();
 
 console.log("Checking store: ", store.getState())
-//TODO: remove this :
 
-// const testData = () => {
-//     for (let i = 0; i < 4; i++) {
-//         store.dispatch(addInstantCoffee({
-//             id: i.toString(),
-//             coffeeName: instantCoffee[i].coffeeName,
-//             packageSize: instantCoffee[i].packageSize,
-//             containerSize: instantCoffee[i].containerSize,
-//             price: instantCoffee[i].price,
-//             currency: instantCoffee[i].currency,
-//             acidity: instantCoffee[i].acidity,
-//             aroma: instantCoffee[i].aroma
-//         }));
-//     }
-// }
-// testData();
-
-console.log("Checking store: (2nd)", store.getState())
 //TODO: remove this :
 const jsx = (
     <Provider store={store}>
