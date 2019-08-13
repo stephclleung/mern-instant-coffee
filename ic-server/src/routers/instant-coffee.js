01ceeeba66d6e3abe3012f4c15a6f85afa57116b
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-12 20:59:18 -0400

    Server: reverting a commit

--- ic-server/src/routers/instant-coffee.js
+++ ic-server/src/routers/instant-coffee.js
@@ -2,7 +2,6 @@ const Coffee = require('../models/instant-coffee');
 const express = require('express');
 const router = new express.Router();
 
-const pseudoCoffee = require('../data/fake-data');
 
 router.get('/', async (req, res) => {
     try {
@@ -17,18 +16,29 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
     try {
         const instantCoffee = await Coffee.findById(req.params.id);
-        res.send(instantCoffee);
+        instantCoffee ? res.status(200).send(instantCoffee) : res.status(404).send();
     } catch (error) {
         console.log("SERVER: error occured at GET /coffee/:id | ", error);
+        req.status(400).send();
     }
+})
 
+router.delete('/:id', async (req, res) => {
+    try {
+        const instantCoffee = await Coffee.findOneAndDelete(req.params.id);
+        res.status(204).send(instantCoffee);
+
+    } catch (error) {
+        console.log("SERVER: error occured at GET /coffee/:id | ", error);
+        res.status(400).send();
+    }
 })
 
 router.post('/', async (req, res) => {
     try {
         const instantCoffee = await new Coffee(req.body).save();
         //TODO: Check duplicates.
-        res.send(instantCoffee);
+        res.status(201).send(instantCoffee);
     } catch (error) {
         console.log("SERVER: error occured at POST /coffee/ ||", error);
         res.send({});
