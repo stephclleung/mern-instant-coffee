f5ac4fb405a9e44c5b7198ca9b24e44f8d00bb35
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-14 23:20:39 -0400

    Actions/filter: two more filters. Tests set up

--- ic-client/src/actions/filter.js
+++ ic-client/src/actions/filter.js
@@ -3,3 +3,12 @@ export const setTextFilter = (text = "") => ({
     type: 'SET_TEXT_FILTER',
     text
 });
+
+export const sortByPrice = () => ({
+    type: 'SORT_BY_PRICE'
+});
+
+export const setCurrencyFilter = (currency = "") => ({
+    type: 'SET_CURRENCY_FILTER',
+    currency
+})
--- ic-client/src/tests/actions/filter.test.js
+++ ic-client/src/tests/actions/filter.test.js
@@ -1,10 +1,25 @@
-import { setTextFilter } from '../../actions/filter';
+import { setTextFilter, sortByPrice, setCurrencyFilter } from '../../actions/filter';
 
 
 test("Should return text filter object", () => {
     const action = setTextFilter("coffee");
     expect(action).toEqual({
-        type: 'SET_TEXT_FILTER',
+        type: "SET_TEXT_FILTER",
         text: "coffee"
+    });
+})
+
+test("Should return sort by price object", () => {
+    const action = sortByPrice();
+    expect(action).toEqual({
+        type: "SORT_BY_PRICE"
+    });
+})
+
+test("Should return currency filter object", () => {
+    const action = setCurrencyFilter("JPY");
+    expect(action).toEqual({
+        type: "SET_CURRENCY_FILTER",
+        currency: "JPY"
     });
 })
