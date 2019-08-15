0eee7321bf61e40eeb7937f5a2e2983fc33c0156
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-15 17:34:17 -0400

    Selectors/Instant-coffee: Fine tuning how to sort the coffees
    Calculates gram/$, converting between currencies, etc

--- ic-client/src/selectors/instant-coffee.js
+++ ic-client/src/selectors/instant-coffee.js
@@ -7,12 +7,48 @@ const getInstantCoffee = (instantCoffees, filter) => {
         switch (filter.sortBy) {
             case 'aroma':
                 return coffeeA.aroma < coffeeB.aroma ? 1 : -1;
+            case 'acidity':
+                return coffeeA.acidity < coffeeB.acidity ? -1 : 1;
+            case 'price':
+                return priceComparison(coffeeA, coffeeB)
+            case 'amount':
+                return amountComparison(coffeeA, coffeeB)
             default:
                 console.log("Sorting: No need")
         }
-
         return 1;
     });
 };
+
+const amountComparison = (coffeeA, coffeeB) => {
+    const coffeeASize = convertAmount(coffeeA);
+    const coffeeBSize = convertAmount(coffeeB);
+    return coffeeASize < coffeeBSize ? 1 : -1;
+}
+
+const convertAmount = (coffee) => {
+    return coffee.packageSize ? coffee.packageSize * coffee.containerSize : coffee.containerSize;
+}
+
+const convertPrice = (coffee) => {
+    switch (coffee.currency) {
+        case "JPY":
+            return coffee.price / 80;
+        case "USD":
+            return coffee.price / 0.75;
+        case "HKD":
+            return coffee.price / 5.8
+        default:
+            return coffee.price
+    }
+}
+
+const priceComparison = (coffeeA, coffeeB) => {
+    const coffeeAPrice = convertPrice(coffeeA) / convertAmount(coffeeA);
+    const coffeeBPrice = convertPrice(coffeeB) / convertAmount(coffeeB)
+
+    return coffeeAPrice < coffeeBPrice ? 1 : -1;
+}
+
 
 export default getInstantCoffee;
