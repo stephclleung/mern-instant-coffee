dfb4ff27aa0d07ccb6d21929fc5bf15c5951942d
stephclleung <35399643+stephclleung@users.noreply.github.com>
2019-08-17 22:52:54 -0400

    Configuring compose
    so it wouldn't crash on browsers that don't have the dev tools.

--- ic-client/src/store/store.js
+++ ic-client/src/store/store.js
@@ -5,14 +5,14 @@ import errorReducer from '../reducers/error';
 import thunk from 'redux-thunk';
 
 export default () => {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
     const store = createStore(
         combineReducers({
             instantCoffees: instantCoffeeReducer,
             filters: instantCoffeeFilterReducer,
             errors: errorReducer
         }),
-        compose(applyMiddleware(thunk), //Middleware for AJAX
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        composeEnhancers(applyMiddleware(thunk)) //middleware for ajax
     );
     return store;
 }
